ae6476082ba6d5cffec29a978e020e9ebcd1dd38
Mudassir Asif Bubere <mudassir@Mudassirs-MacBook-Air.local>
2026-03-27 01:14:08 +0530

    Updated phone number and cart logic

--- index.js
+++ index.js
@@ -3,16 +3,15 @@ let totalAmount = 0;
 
 $(document).ready(function() {
 
+  // Navbar Adjustment
   if ($(document).width() <= 992) {
-    $('.navbar-nav').removeClass("ml-auto");
-    $('.navbar-nav').addClass("mr-auto");
+    $('.navbar-nav').removeClass("ml-auto").addClass("mr-auto");
   } else {
-    $('.navbar-nav').removeClass("mr-auto");
-    $('.navbar-nav').addClass("ml-auto");
+    $('.navbar-nav').removeClass("mr-auto").addClass("ml-auto");
   }
 
+  // Scroll to Top Logic
   var scrollToTopBtn = $('#scrollToTop');
-
   $(window).scroll(function() {
     if ($(window).scrollTop() > 300) {
       scrollToTopBtn.addClass('show');
@@ -23,248 +22,125 @@ $(document).ready(function() {
 
   scrollToTopBtn.on('click', function(event) {
     event.preventDefault();
-    $('html, body').animate({
-      scrollTop: 0
-    }, '500');
+    $('html, body').animate({ scrollTop: 0 }, '500');
   });
 
-  $(".navbar a").on('click', function(event) {
-    // Make sure this.hash has a value before overriding default behavior
+  // Smooth Scrolling
+  $(".navbar a, .homeBtn").on('click', function(event) {
     if (this.hash !== "") {
-      // console.log(this);
-      // console.log(this.hash);
-      // Prevent default anchor click behavior
       event.preventDefault();
-      // Store hash
       var hash = this.hash;
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
       $('html, body').animate({
         scrollTop: $(hash).offset().top
       }, 800, function() {
-        // Add hash (#) to URL when done scrolling (default click behavior)
-        window.location.hash = hash;
-      });
-
-    } // End if
-  });
-
-  $('.homeBtn').click(function(event) {
-    if (this.hash !== "") {
-      event.preventDefault();
-      let hash = this.hash;
-
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 800, function() {
-        // Add hash (#) to URL when done scrolling (default click behavior)
         window.location.hash = hash;
       });
     }
   });
 
+  // Category Filtering
   $('.product-box-layout4').click(function() {
     $(this).toggleClass("productClicked").parent().siblings('div').children().removeClass("productClicked");
-    if ($(this)[0].className.search('momos productClicked') > -1) {
-      $('#momos').show().siblings('div').hide();
+    let targetId = "";
+    if ($(this).hasClass('momos')) targetId = '#momos';
+    else if ($(this).hasClass('chinese')) targetId = '#chinese';
+    else if ($(this).hasClass('beverages')) targetId = '#beverages';
 
-      $('html, body').animate({
-        scrollTop: $('#momos').offset().top
-      }, 800, function() {});
-
-    } else if ($(this)[0].className.search('chinese productClicked') > -1) {
-      $('#chinese').show().siblings('div').hide();
-
-      $('html, body').animate({
-        scrollTop: $('#chinese').offset().top
-      }, 800, function() {});
-
-    } else if ($(this)[0].className.search('beverages productClicked') > -1) {
-      $('#beverages').show().siblings('div').hide();
-
-      $('html, body').animate({
-        scrollTop: $('#beverages').offset().top
-      }, 800, function() {});
+    if (targetId) {
+      $(targetId).show().siblings('div').hide();
+      $('html, body').animate({ scrollTop: $(targetId).offset().top }, 800);
     }
   });
 
-
+  // Menu Increment/Decrement
   $(".menuBtn").click(function() {
-    let quantity = $(this).siblings(".quantity");
-    let foodNameClicked = quantity.parent().siblings('div').children().first().text().trim();
-    let singleFoodAmount = Number(quantity.parent().siblings('div').children().last().text());
+    let quantityLabel = $(this).siblings(".quantity");
+    let foodName = quantityLabel.parent().siblings('div').children().first().text().trim();
+    let price = Number(quantityLabel.parent().siblings('div').children().last().text());
+    let count = Number(quantityLabel.text());
 
-    let count = Number(quantity.text());
-    if ($(this)[0].className.search('plus') > -1) {
-      count = count + 1;
-      quantity.text(count);
-      ToCart(foodNameClicked, count, singleFoodAmount);
-
-    } else if ($(this)[0].className.search('minus') > -1) {
-      if (count <= 0) {
-        quantity.text(0);
-      } else {
-        count = count - 1;
-        quantity.text(count);
-        ToCart(foodNameClicked, count, singleFoodAmount);
-      }
+    if ($(this).hasClass('plus')) {
+      count++;
+    } else if ($(this).hasClass('minus') && count > 0) {
+      count--;
     }
+    
+    quantityLabel.text(count);
+    updateCart(foodName, count, price);
   });
 
-  function ToCart(foodNameClicked, foodQuantity, singleFoodAmount) {
-    let foodAlreadyThere = false;
-    let foodPos;
-
-    for (var i = 0; i < food.length; i++) {
-      if (food[i][0] === foodNameClicked) {
-        foodAlreadyThere = true;
-        foodPos = i;
-        break;
-      } else {
-        foodAlreadyThere = false;
-      }
+  function updateCart(name, qty, price) {
+    // Find if item exists
+    let index = food.findIndex(item => item[0] === name);
+    
+    if (index > -1) {
+      if (qty === 0) food.splice(index, 1);
+      else food[index][1] = qty;
+    } else if (qty > 0) {
+      food.push([name, qty, price]);
     }
 
-    if (foodAlreadyThere) {
+    // UI Update
+    let cartDiv = $('.cartContentDiv');
+    cartDiv.empty();
+    totalAmount = 0;
 
-      food.splice(foodPos, 1);
-      food.push([foodNameClicked, foodQuantity, singleFoodAmount]);
-    } else {
-      food.push([foodNameClicked, foodQuantity, singleFoodAmount]);
-    }
-
-    // Remove Food items with quantity = 0
-    for (var i = 0; i < food.length; i++) {
-      if (food[i][1] === 0) {
-        food.splice(i, 1);
-      }
-    }
-
-    if (food.length !== 0) {
+    if (food.length > 0) {
       $('.shoppingCart').addClass('shoppingCartWithItems');
-
-      $('.cartContentDiv').empty();
-      for (var i = 0; i < food.length; i++) {
-        let cartTxt = '<div class="row cartContentRow"><div class="col-10"> <p>' + food[i][0] + ' </p> <p class="text-muted-small"><i class="fas fa-rupee-sign"></i>' + food[i][2] + '</p>  </div>  <div class="col-2">  <span class="cartQuantity">' + food[i][1] + '</span> <p class="text-muted-small"><i class="fas fa-rupee-sign"></i>' + food[i][1] * food[i][2] + '</p>   </div>  </div> <hr class="cartHr">';
-        $('.cartContentDiv').append(cartTxt);
-      }
-
+      food.forEach(item => {
+        totalAmount += (item[1] * item[2]);
+        cartDiv.append(`
+          <div class="row cartContentRow">
+            <div class="col-10">
+              <p>${item[0]}</p>
+              <p class="text-muted-small"><i class="fas fa-rupee-sign"></i>${item[2]}</p>
+            </div>
+            <div class="col-2 text-right">
+              <span class="cartQuantity">${item[1]}</span>
+              <p class="text-muted-small"><i class="fas fa-rupee-sign"></i>${item[1] * item[2]}</p>
+            </div>
+          </div><hr class="cartHr">`);
+      });
     } else {
       $('.shoppingCart').removeClass('shoppingCartWithItems');
-
-      $('.cartContentDiv').empty();
-      $('.cartContentDiv').append('<h1 class="text-muted">Your Cart is Empty</h1>');
+      cartDiv.append('<h1 class="text-muted">Your Cart is Empty</h1>');
     }
 
     $('.shoppingCartAfter').text(food.length);
-
-    if (food.length === 0) {
-      totalAmount = 0;
-    } else {
-      totalAmount = totalAmount + singleFoodAmount;
-    }
-
-    $('.totalAmountDiv').empty();
-    $('.totalAmountDiv').append('Total Amount: ' + totalAmount);
-
+    $('.totalAmountDiv').html('Total Amount: <i class="fas fa-rupee-sign"></i>' + totalAmount);
   }
-
-
 });
 
+// FIXED WHATSAPP REDIRECT
 function openWhatsapp() {
+  let addressField = $('#address').val();
+  let noteField = $('#note').val();
 
-  // console.log($('#address'));
-
-  if ($('#address')[0].value === "") {
+  if (!addressField) {
     alert("Please Enter Address");
-    return
-
-  } else {
-    let total = 0;
-    let address = $('#address')[0].value;
-    let note = $('#note')[0].value;
-    let wTxt = '*name*               *quantity* \n';
-
-    for (var i = 0; i < food.length; i++) {
-      let name = food[i][0];
-      let quantity = food[i][1];
-      total = total + food[i][1] * food[i][2];
-      wTxt = wTxt + name + '      ' + quantity + '  \n';
-    }
-
-    if ($('#note')[0].value === "") {
-      wTxt = wTxt + '\n *Total Bill: ' + total + '*' + '\n\n Address: ' + address;
-    } else {
-      wTxt = wTxt + '\n *Total Bill: ' + total + '*' + '\n\n Address: ' + address + '\n Note: ' + note;
-    }
-
-    let wTxtEncoded = encodeURI(wTxt);
-    window.open("https://wa.me/917719077157?text=" + wTxtEncoded);
+    return;
+  }
+  
+  if (food.length === 0) {
+    alert("Your cart is empty!");
+    return;
   }
 
+  let wTxt = '*New Order from Momo Agency*\n\n';
+  wTxt += '*Item Name* - *Qty*\n';
+  wTxt += '--------------------------\n';
 
+  let finalTotal = 0;
+  food.forEach(item => {
+    wTxt += `${item[0]} x ${item[1]}\n`;
+    finalTotal += (item[1] * item[2]);
+  });
+
+  wTxt += `\n*Total Bill: ₹${finalTotal}*`;
+  wTxt += `\n\n*Delivery Address:* ${addressField}`;
+  if (noteField) wTxt += `\n*Note:* ${noteField}`;
+
+  let encodedMsg = encodeURIComponent(wTxt);
+  // YOUR NUMBER UPDATED HERE
+  window.open(`https://wa.me/917719077157?text=${encodedMsg}`, "_blank");
 }
-
-// Did not work out code
-// let totalAmount = 0;
-// let foodNameClickedArray = [];
-// let singleFoodAmountArray = [];
-// let foodQuantityArray = [];
-//
-// function addToCart(foodNameClicked, foodQuantity, singleFoodAmount) {
-//
-//   if (foodNameClickedArray.indexOf(foodNameClicked) > -1) {
-//
-//     foodNameClickedArray.splice(foodNameClickedArray.indexOf(foodNameClicked), 1);
-//     foodNameClickedArray.push(foodNameClicked.trim());
-//
-//     if (singleFoodAmountArray.indexOf(singleFoodAmount) > -1) {
-//       singleFoodAmountArray.splice(singleFoodAmountArray.indexOf(singleFoodAmount), 1);
-//       singleFoodAmountArray.push(singleFoodAmount);
-//     }
-//
-//     if (foodQuantityArray.indexOf(foodQuantity - 1) > -1) {
-//       foodQuantityArray.splice(foodQuantityArray.indexOf(foodQuantity - 1), 1);
-//       foodQuantityArray.push(foodQuantity);
-//     }
-//
-//   } else {
-//     foodNameClickedArray.push(foodNameClicked.trim());
-//     singleFoodAmountArray.push(singleFoodAmount);
-//     foodQuantityArray.push(foodQuantity);
-//   }
-//
-//   totalAmount = totalAmount + singleFoodAmount;
-//
-//   console.log(foodNameClickedArray);
-//   console.log(foodQuantityArray);
-//   console.log(singleFoodAmountArray);
-//   console.log('Total Bill: ', totalAmount);
-// }
-
-
-// function removeFromCart(foodNameClicked, foodQuantity, singleFoodAmount) {
-//   if (foodQuantity > 0) {
-//     if (foodQuantityArray.indexOf(foodQuantity + 1) > -1) {
-//       console.log(foodQuantityArray.indexOf(foodQuantity + 1));
-//
-//       foodQuantityArray.splice(foodQuantityArray.indexOf(foodQuantity + 1), 1);
-//       foodNameClickedArray.splice(foodQuantityArray.indexOf(foodQuantity + 1), 1);
-//       singleFoodAmountArray.splice(foodQuantityArray.indexOf(foodQuantity + 1), 1);
-//
-//       foodQuantityArray.push(foodQuantity);
-//       foodNameClickedArray.push(foodNameClicked);
-//       singleFoodAmountArray.push(singleFoodAmount);
-//     }
-//
-//   }
-//
-//   totalAmount = totalAmount - singleFoodAmount;
-//
-//
-//   console.log(foodNameClickedArray);
-//   console.log(foodQuantityArray);
-//   console.log(singleFoodAmountArray);
-//   console.log('Total Bill: ', totalAmount);
-// }
